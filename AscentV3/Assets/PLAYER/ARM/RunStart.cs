26842513e95f21a2bc1ca2a9a5453828f900e610
MrBlackBaba <deathbaba@hotmail.fr>
2019-04-04 18:57:26 +0200

    Bras + Light Tower Int.
    Mis en place des bras, début des lights à l'int. de la tour, léger fix des zone anti grav

---
+++ AscentV3/Assets/PLAYER/ARM/RunStart.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunStart : MonoBehaviour
+{
+    public bool a;
+    private Animator Anim;
+
+
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+        
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Anim.SetBool("Walk", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            Anim.SetBool("Walk", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Anim.SetBool("Saut", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Anim.SetBool("Saut", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Anim.SetBool("Glis", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            Anim.SetBool("Glis", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Anim.SetBool("Power", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Anim.SetBool("Power", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Anim.SetBool("Walk2", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            Anim.SetBool("Walk2", false);
+        }
+    }
+}
